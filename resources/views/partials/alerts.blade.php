ebe739959a670279f1fc85441c2b39ed5ba6196c
EdgarB05 <bgeo230010@upemor.edu.mx>
2026-03-28 23:19:06 -0600

    Alertas

--- resources/views/partials/alerts.blade.php
+++ resources/views/partials/alerts.blade.php
@@ -21,8 +21,25 @@
 @endif
 
 @if (session('error'))
-    <div class="alert alert-danger">
-        {{ session('error') }}
+
+    <div class="alert alert-danger alert-dismissible d-flex align-items-center fade show">
+        <i class="fa-solid fa-circle-exclamation"></i>
+        <strong class="mx-2">Error!</strong> {{ session('error') }}
+        <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
     </div>
+
+    <script>
+        setTimeout(function() {
+            let alerta = document.getElementById('alert');
+            if (alerta) {
+                alerta.classList.remove('show');
+
+                alerta.classList.add('fade');
+
+                setTimeout(() => alerta.remove(), 500);
+            }
+            
+        }, 5000);
+    </script>
 @endif
 
